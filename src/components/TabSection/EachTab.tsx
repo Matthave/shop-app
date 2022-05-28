416aa9c8e26bb4e9f5c4bdb3d640a921671fa995
Matthave <matthevr@gmail.com>
2022-05-28 13:15:33 +0200

    shopApp/tabSection-v2 Change structure of tabs data, fix issue with display tab

--- src/components/TabSection/EachTab.tsx
+++ src/components/TabSection/EachTab.tsx
@@ -1,24 +1,20 @@
-import { useState } from "react";
 
 const EachTab: React.FC<{
     currentMonthAndDay: any, 
     category: any,
-}> = ({currentMonthAndDay, category}) => {
-    const [ showCurrentTab, setShowCurrentTab ] = useState(false);
+    currentTabHandler: (tabName: string) => void,
+}> = ({currentMonthAndDay, category, currentTabHandler}) => {
     const { currentDay } = currentMonthAndDay;
 
-    const showCurrentTabfunc = () => {
-        setShowCurrentTab((prevState) => {
-            return !prevState;
-        })
-    }
-
     const { name: categoryName, color: categoryColor } =  currentDay[category.name];
+    const { name: tabName, url, active: tabActive } = category;
     return (
         <>
             {categoryName !== '' && 
-                <div style={{backgroundColor: categoryColor}} className={`tab__category ${showCurrentTab ? 'tab__category--active' : ''}`} onClick={showCurrentTabfunc}>
-                    {category.name} 
+                <div style={{backgroundColor: categoryColor}} 
+                className={`tab__category ${tabActive ? 'tab__category--active' : ''}`} 
+                onClick={() => currentTabHandler(tabName)}>
+                    {tabName} 
                     <div style={{backgroundColor: categoryColor}} className='tab__recipe'></div>
                 </div>
             }
