d4beff2efbb2bf540d9a9298f5f38d224c70308b
Matthave <matthevr@gmail.com>
2022-06-12 02:12:09 +0200

    shopApp/ingredients Add new endpoint for generate all ingredients from selected meals

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import React, { useState, useReducer, useCallback } from 'react';
 import "./css/styles.min.css"
 import Calendar from './components/Calendar/Calendar';
 import Modal from './components/Modal/Modal';
-import { ModalPropMonthData, ModalPropMonthArrData, NewMeal } from './Types/types';
+import { ModalPropMonthData, ModalPropMonthArrData, NewMeal, IngredientsType } from './Types/types';
 import { clearActiveDays } from './utils/clearActiveDays';
 import TabSection from './components/TabSection/TabSection';
 import Generate from './components/Generate/Generate';
+import Ingredients from './components/Ingredients/Ingredients';
 
 const modalInitialState = {
 modalVisibility: false,
@@ -27,6 +28,7 @@ const App: React.FC = () => {
   const [modalVisibility, setModalVisibility ] = useState(false);
   const [modalData, disptachModalData] = useReducer(modalReducer, modalInitialState);
   const [chosenMeals, setChosenMeal] = useState <NewMeal[]>([]);
+  const [ingredients, setIngredients] = useState<IngredientsType[]>([]);
   const modalVisibilityHandler = useCallback((clickedMonthData: ModalPropMonthData, monthData: ModalPropMonthArrData) => {
     disptachModalData({type: 'updateData', payload: {clickedMonthData, monthData}})
     const asyncShowModal = async () => {
@@ -43,12 +45,17 @@ const App: React.FC = () => {
     setMonthsArr(clearActiveDays(monthsArr));
   }
 
+  const ingredientsLoaderFunc = (ing: []) => {
+    setIngredients(ing)
+  }
+
   return (
     <div className="App">
       <Calendar modalVisibilityHandler={modalVisibilityHandler} setModalVisibility={setModalVisibility}/>
       {modalVisibility && <Modal modalData={modalData} closeButton={closeButton} setChosenMeal={setChosenMeal} chosenMeals={chosenMeals}/>}
       {modalVisibility && <TabSection modalData={modalData}/>}
-      <Generate chosenMeals={chosenMeals}/>
+      <Generate chosenMeals={chosenMeals} ingredientsLoaderFunc={ingredientsLoaderFunc}/>
+      <Ingredients ingredients={ingredients}/>
     </div>
   );
 }
